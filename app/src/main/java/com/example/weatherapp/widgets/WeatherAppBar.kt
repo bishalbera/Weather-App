1c6f7e2dad71b79662e900f1bfb4eb0e7a8590a4
Bishal <bishalbera2@gmail.com>
2023-04-15 16:38:50 +0530

    more opt button added

--- app/src/main/java/com/example/weatherapp/widgets/WeatherAppBar.kt
+++ app/src/main/java/com/example/weatherapp/widgets/WeatherAppBar.kt
@@ -1,12 +1,17 @@
 package com.example.weatherapp.widgets
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.FavoriteBorder
+import androidx.compose.material.icons.filled.Info
 import androidx.compose.material.icons.filled.Search
+import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material.icons.rounded.MoreVert
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
@@ -30,6 +35,14 @@ fun WeatherAppBar(
     onButtonClicked: () -> Unit = {}
 
 ) {
+    val showDialog = remember {
+        mutableStateOf(false)
+    }
+    if(showDialog.value) {
+        ShowSettingDropDown(showDialog = showDialog, navController = navController)
+    }
+
+
     TopAppBar(title = {Text(text = title,
                         color = MaterialTheme.colors.onSecondary,
                         style = TextStyle(fontWeight = FontWeight.Bold,
@@ -41,7 +54,9 @@ fun WeatherAppBar(
                          Icon(imageVector = Icons.Default.Search, contentDescription = "search icon" )
 
                       }
-                      IconButton(onClick = { /*TODO*/ }) {
+                      IconButton(onClick = {
+                          showDialog.value = true
+                      }) {
                           Icon(imageVector = Icons.Rounded.MoreVert, contentDescription = "More Icon")
 
                       }
@@ -60,5 +75,49 @@ fun WeatherAppBar(
         backgroundColor = Color.Transparent,
         elevation = elevation)
 
+
+}
+
+@Composable
+fun ShowSettingDropDown(showDialog: MutableState<Boolean>,
+                        navController: NavController) {
+    var expanded by remember {
+        mutableStateOf(true)
+    }
+    val items = listOf("Favourites", "Settings", "About")
+    Column(modifier = Modifier
+        .fillMaxWidth()
+        .wrapContentSize(Alignment.TopEnd)
+        .absolutePadding(top = 40.dp, right = 20.dp)) {
+        DropdownMenu(expanded = expanded,
+            onDismissRequest = {  expanded = false },
+            modifier = Modifier
+                .width(140.dp)
+                .background(Color.White)) {
+
+            items.forEachIndexed{
+                index, text ->
+                DropdownMenuItem(onClick = {
+                    expanded = false
+                    showDialog.value = false
+                }) {
+                    Icon(
+                        imageVector = when (text) {
+                                                  "About" -> Icons.Default.Info
+                                                   "Favourites" -> Icons.Default.FavoriteBorder
+                                                    else -> Icons.Default.Settings
+                        }, contentDescription = null,
+                        tint = Color.LightGray
+                    )
+                    Text(text = text, modifier = Modifier.clickable {  },
+                            fontWeight = FontWeight.W300)
+
+
+                }
+            }
+            
+        }
+
+    }
 
 }
